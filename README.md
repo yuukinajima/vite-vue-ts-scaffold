0169b9445cfd7e934fdd10614f8d0445197f7a27
yuki najima <y.najima@kumonosudou.com>
2023-06-13 15:12:03 +0900

    add 404

--- README.md
+++ README.md
@@ -3,21 +3,18 @@
 ## Project setup
 
 ```
-# npm
 npm install
 ```
 
 ### Compiles and hot-reloads for development
 
 ```
-# npm
 npm run dev
 ```
 
 ### Compiles and minifies for production
 
 ```
-# npm
 npm run build
 ```
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,62 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { resolve } from "path";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({ 
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    base: process.env.VITE_APP_PATH,
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          "404": resolve(__dirname, "404.html"),
+        },
+      },
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
+    plugins: [
+      htmlPlugin(loadEnv(mode, ".")),
+      vue({
+        template: { transformAssetUrls },
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+      }),
     ],
-  },
-  server: {
-    port: 3000,
-  },
-})
+    define: { "process.env": {} },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+    },
+    server: {
+      port: 3000,
+    },
+  };
+});
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: "html-transform",
+    transformIndexHtml: {
+      order: "pre" as const,
+      handler: (html: string, { path }: { path: string }): string => {
+        if (path == "/404.html") {
+          return html.replace(/<%=SEGMENT_COUNT%>/g, () => {
+            return env.VITE_SEGMENT_COUNT;
+          });
+        }
+        return html;
+      },
+    },
+  };
+}
